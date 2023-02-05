505091c53c43dee9ade79ed5d83ee1258e76e1d3
QaiserHussain <qaiserh844@gmail.com>
2023-02-05 19:05:00 +0500

    change input fields

--- components/TableList.jsx
+++ components/TableList.jsx
@@ -12,14 +12,25 @@ function TableList({ styles, isDel, setDel, item, index, workingDays, handleChan
             <div className={styles.inputs}>
                 {
                     workingDays.map((value, i) => (
-                        <input
-                            checked={item.attendance[i] === 'present' ? true : item.attendance[i] === 'abscent' ? true : null}
-                            style={{ accentColor: item.attendance[i] === 'abscent' ? 'red' : item.attendance[i] === 'present' ? 'green' : 'blue' }}
-                            key={i}
-                            type="radio"
-                            value={'present'}
-                            // disabled={item.attendance[index] === 'present' || 'abscent' ? true : false}
-                            onChange={(e) => { handleChange(item._id, e.target.value, i) }} />
+                        <select name="" id=""
+                         value={item.attendance[i] === 'present' ? 'P': item.attendance[i] === 'abscent' ? 'A' : '...'  }
+                         onChange={(e) => { handleChange(item._id, e.target.value, i)}}
+                         disabled={item.attendance[i] === 'present' ? true : item.attendance[i] === 'abscent' ? true: false}
+                         style={{color:item.attendance[i] === 'present' ? 'green' : item.attendance[i] === 'abscent' ? 'red': 'black',border:item.attendance[i] === 'present' ? '2px solid green' : item.attendance[i] === 'abscent' ? '2px solid red': '2px solid black'}}
+                         >
+                            <option value={item.attendance[i] === 'present' ? 'present': item.attendance[i] === 'abscent' ? 'abscent' : '...'  }>{item.attendance[i] === 'present' ? 'P': item.attendance[i] === 'abscent' ? 'A' : '...'  }</option>
+                            <option value="present">P</option>
+                            <option value="abscent">A</option>
+                        </select>
+
+                        // <input
+                        //     checked={item.attendance[i] === 'present' ? true : item.attendance[i] === 'abscent' ? true : null}
+                        //     style={{ accentColor: item.attendance[i] === 'abscent' ? 'red' : item.attendance[i] === 'present' ? 'green' : 'blue' }}
+                        //     key={i}
+                        //     type="radio"
+                        //     value={'present'}
+                        //     disabled={item.attendance[i] === 'present' ? true : false}
+                        //     onChange={(e) => { handleChange(item._id, e.target.value, i) }} />
                     ))
                 }
             </div>
